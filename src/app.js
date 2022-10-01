03ddc84ca1e0fb3a52d01f2da7cacd056727a53a
AmbujGuptaConeio <ambuj@coneio.com>
2022-10-01 10:55:16 +0530

    Heroku Ready

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const publicDirPath = path.join(__dirname, '../public')
 const templatesPath = path.join(__dirname, '../templates/views')
@@ -81,6 +82,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server Started on Port 3000.')
+app.listen(port, () => {
+    console.log('Server Started on Port ' + port)
 })
